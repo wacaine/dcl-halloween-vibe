e0d21b3446bf699bb493cae711f7a0b5cd131b07
William Caine <william.caineiii@gmail.com>
2025-10-28 18:48:54 -0400

    initial scene => gnark

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gnark (SDK7 Version)
+
+This scene has no description yet.
+
+> Please visit the [SDK7 scene template](https://github.com/decentraland/sdk7-scene-template) for a SDK7 summary.
+
+## Try it out
+
+**Previewing the scene**
+
+1. Download this full repository from [sdk7-goerli-plaza](https://github.com/decentraland/sdk7-goerli-plaza/tree/main), including this and several other example scenes on SDK7.
+1. Download this repository.
+
+2. Open the command line and navigate to this scene root directory
+
+3. Run:
+
+```
+npm install
+```
+
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
+import { Vector3 } from '@dcl/sdk/math'
+
+import { createGnark } from './gnark'
+
+import { distanceSystem, walkAround } from './systems/gnarkAI'
+import { setupUi } from './ui'
+
+export function main() {
+  const temple = engine.addEntity()
+
+  Transform.create(temple, {
+    position: Vector3.create(16, 0, 16),
+    scale: Vector3.create(1.6, 1.6, 1.6)
+  })
+
+  GltfContainer.create(temple, {
+    src: 'assets/scene/Models/Temple.glb'
+  })
+
+  createGnark(0)
+  createGnark(1)
+
+  // UI with GitHub link
+  setupUi()
+}
+
+engine.addSystem(walkAround)
+engine.addSystem(distanceSystem)
